822c46623131a9ab69ad9564ce7f8eae9af9c238
Terence Chan Zun Mun <zunmun@gmail.com>
2026-06-24 00:19:22 +0200

    feat: add watchdog chall to dist

--- greymechaarmy_v2/firmware/rp2350/filesystem/challs/fast_secure_memory/template.py
+++ greymechaarmy_v2/firmware/rp2350/filesystem/challs/fast_secure_memory/template.py
@@ -10,17 +10,11 @@ import hardware.fpga
 #import rp2pio
 
 def setup_fpga():
-    bitstream = "/tmp/main.bit" if "main.bit" in os.listdir("/tmp") else "/hardware/bitstreams/main.bit"
-    if bitstream == "/tmp/main.bit" or "main.bit" in os.listdir("/hardware/bitstreams"):
-        print("Uploading", bitstream)
-        rst = hardware.fpga.upload_bitstream(bitstream)
-        time.sleep(0.25)
-        return rst
-    else:
-        print("No main.bit found; using current FPGA bitstream")
-        return None
+    rst = hardware.fpga.upload_bitstream("/challs/fast_secure_memory/main.bit")
+    return rst
 
 fpga_rst = setup_fpga()
 
 
 ### Setup your code here ##################
+
